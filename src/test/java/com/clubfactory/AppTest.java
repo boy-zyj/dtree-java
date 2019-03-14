01f5f77398e5adf4445b75735189de9ff3903745
yaojunzou <yaodeshiluo@gmail.com>
2019-03-14 16:31:19 +0800

    [add]添加测试unit

--- src/test/java/com/clubfactory/AppTest.java
+++ src/test/java/com/clubfactory/AppTest.java
@@ -4,6 +4,70 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import com.clubfactory.dtree.core.*;
+
+class Data implements Center {
+    String country;
+    String payType;
+    boolean valid = true;
+
+    Data(String country, String payType) {
+        this.country = country;
+        this.payType = payType;
+    }
+}
+
+class IsIndia extends Condition {
+    @Override
+    public boolean validate(Center center) {
+        Data data = (Data) center;
+        return data.country.equals("india");
+    }
+}
+
+class IsCod extends Condition {
+    @Override
+    public boolean validate(Center center) {
+        Data data = (Data) center;
+        return data.payType.equals("cod");
+    }
+}
+
+class IsValid extends Condition {
+    @Override
+    public boolean validate(Center center) {
+        Data data = (Data) center;
+        return data.valid;
+    }
+}
+
+class ToHn extends Action {
+    @Override
+    public void run(Center center) {
+        Data data = (Data) center;
+        System.out.println("to hn");
+    }
+}
+
+class ToXs extends Action {
+    @Override
+    public void run(Center center) {
+        Data data = (Data) center;
+        System.out.println("to xs");
+    }
+}
+
+class MyDTree extends DTree {
+
+    public MyDTree(Node node) {
+        super(node);
+    }
+
+    public DTree newInstance(Node node) {
+        return new MyDTree(node);
+    }
+}
+
 /**
  * Unit test for simple App.
  */
@@ -31,8 +95,30 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testDTree()
     {
-        assertTrue( true );
+        Data data = new Data("india", "prepay");
+        IsIndia isIndia = new IsIndia();
+        IsCod isCod = new IsCod();
+        IsValid isValid = new IsValid();
+
+        ToHn toHn = new ToHn();
+        ToXs toXs = new ToXs();
+        Node node = new Node(
+                new T(isCod, toHn),
+                new T(isIndia, new Node(
+                        new T(isValid, toXs),
+                        new T(new Else(), toHn)
+                )),
+                new T(new Else(), toXs)
+        );
+        MyDTree tree = new MyDTree(node);
+        System.out.println(tree);
+        try {
+            tree.run(data);
+        } catch (NoMatchException e) {
+            System.out.println(e);
+        }
+
     }
 }
