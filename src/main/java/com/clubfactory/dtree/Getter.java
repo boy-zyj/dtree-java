8ec998147e517380794a4e541aeacdfa9d23dfb9
yaojunzou <yaodeshiluo@gmail.com>
2019-03-20 08:44:20 +0800

    fix

--- src/main/java/com/clubfactory/dtree/Getter.java
+++ src/main/java/com/clubfactory/dtree/Getter.java
@@ -1,8 +1,20 @@
 package com.clubfactory.dtree;
 
+/**
+ * 从INPUT中获得OUTPUT类型的值
+ *
+ * @author yao
+ * @date 2019/03/19
+ */
 @FunctionalInterface
 public interface Getter<INPUT, OUTPUT> {
 
-    public OUTPUT get(INPUT input);
+    /**
+     * 从INPUT中获得OUTPUT类型的值
+     *
+     * @param input INPUT类型
+     * @return OUTPUT
+     */
+    OUTPUT get(INPUT input);
 
 }
--- src/main/java/com/clubfactory/dtree/NoMatchException.java
+++ src/main/java/com/clubfactory/dtree/NoMatchException.java
@@ -1,3 +1,9 @@
 package com.clubfactory.dtree;
 
+/**
+ * 该异常表示没有找到对应的触发动作(AbstractRunner)
+ *
+ * @author yao
+ * @date 2019/03/19
+ */
 public class NoMatchException extends Exception {}
