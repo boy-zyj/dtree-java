697907fcd26d94ebbce3f54c4c48e18516345f5e
yaojunzou <yaodeshiluo@gmail.com>
2019-12-01 10:39:08 +0800

    fix

--- src/main/java/com/clubfactory/dtree/Action.java
+++ src/main/java/com/clubfactory/dtree/Action.java
@@ -2,7 +2,7 @@ package com.clubfactory.dtree;
 
 public class Action<T> extends AbstractRunner<T> {
 
-    public static Action<?> PASS = new Action<>();
+    public static final Action<?> PASS = new Action<>();
 
     @Override
     public void run(T target) throws NoMatchException {}
