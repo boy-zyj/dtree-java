deca3244261e25be710950c712cc39d4ada36cfb
yaojunzou <yaodeshiluo@gmail.com>
2019-12-09 16:21:42 +0800

    fix Runner

--- src/main/java/com/clubfactory/dtree/Runner.java
+++ src/main/java/com/clubfactory/dtree/Runner.java
@@ -8,27 +8,27 @@ public interface Runner<T> extends Description {
     void run(T target) throws NoMatchException;
 
     default Chain<T> then(Runner<T> runner) {
-        return new Chain<T>(this, runner);
+        return new Chain<>(this, runner);
     }
 
     default Capture<T> capture(Runner<T> onRejectedRunner) {
-        return new Capture<T>(this, onRejectedRunner);
+        return new Capture<>(this, onRejectedRunner);
     }
 
     default Capture<T> capture(BiConsumer<T, RuntimeException> onRejectedErrorHandler) {
-        return new Capture<T>(this, onRejectedErrorHandler);
+        return new Capture<>(this, onRejectedErrorHandler);
     }
 
     default Capture<T> capture(Consumer<RuntimeException> onRejectedErrorHandler) {
-        return new Capture<T>(this, (obj, e) -> onRejectedErrorHandler.accept(e));
+        return new Capture<>(this, (obj, e) -> onRejectedErrorHandler.accept(e));
     }
 
     default Capture<T> capture(Runner<T> onRejectedRunner, BiConsumer<T, RuntimeException> onRejectedErrorHandler) {
-        return new Capture<T>(this, onRejectedRunner, onRejectedErrorHandler);
+        return new Capture<>(this, onRejectedRunner, onRejectedErrorHandler);
     }
 
     default Capture<T> capture(Runner<T> onRejectedRunner, Consumer<RuntimeException> onRejectedErrorHandler) {
-        return new Capture<T>(this, onRejectedRunner, (obj, e) -> onRejectedErrorHandler.accept(e));
+        return new Capture<>(this, onRejectedRunner, (obj, e) -> onRejectedErrorHandler.accept(e));
     }
 
 }
