18d257c90c35df509773af64b87be132b29ef8fd
yaojunzou <yaodeshiluo@gmail.com>
2020-03-26 11:32:36 +0800

    If has public constructors

--- src/main/java/com/clubfactory/dtree/If.java
+++ src/main/java/com/clubfactory/dtree/If.java
@@ -6,12 +6,12 @@ public class If<T> {
     Node<T> node;
     Runner<T> runner;
 
-    If(Condition<T> condition, Node<T> node) {
+    public If(Condition<T> condition, Node<T> node) {
         this.condition = condition;
         this.node = node;
     }
 
-    If(Condition<T> condition, Runner<T> runner) {
+    public If(Condition<T> condition, Runner<T> runner) {
         this.condition = condition;
         this.runner = runner;
     }
