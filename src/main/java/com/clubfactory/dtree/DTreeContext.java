67fdd8022958e5c6884450f1c4ddd204dc667ab0
yaojunzou <yaodeshiluo@gmail.com>
2020-03-26 10:57:09 +0800

    fix

--- src/main/java/com/clubfactory/dtree/DTreeContext.java
+++ src/main/java/com/clubfactory/dtree/DTreeContext.java
@@ -26,11 +26,11 @@ public class DTreeContext<T> {
         return new Node<>(ifs);
     }
 
-    public If<T> jf(Condition<T> condition, Runner<T> runner) {
+    public If<T> lf(Condition<T> condition, Runner<T> runner) {
         return new If<>(condition, runner);
     }
 
-    public If<T> jf(Condition<T> condition, Node<T> node) {
+    public If<T> lf(Condition<T> condition, Node<T> node) {
         return new If<>(condition, node);
     }
 
